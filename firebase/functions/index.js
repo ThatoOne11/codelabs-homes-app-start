2e981b6b002b1dec3edc679c7678d68b78808bb4
Thato <thato@oneeleven.tech>
2026-02-02 11:47:29 +0200

    test

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -3,5 +3,5 @@ const logger = require("firebase-functions/logger");
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!"); //scrub
 });
